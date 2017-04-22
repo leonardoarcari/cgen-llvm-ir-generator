19889afec065ec9a5b0d734653ad7989e0e3c7a1
Leonardo Arcari <leonardo.arcari@mail.polimi.it>
2017-04-22 17:51:27 +0200

    [Registers] Sketches of wanna-be generated files

---
+++ pseudo-generated/registers.h
@@ -0,0 +1,8 @@
+// Includes here
+#include "llvm/IR/GlobalVariable.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+void createRegisters(llvm::Module &M) { 
+	new llvm::GlobalVariable() 
+}

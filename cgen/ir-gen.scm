0de5f09b9c6191953b6656c52000295b523e58b4
Leonardo Arcari <leonardo.arcari@mail.polimi.it>
2017-04-26 16:54:04 +0200

    [Utils] Added new convenience procedures - Run ifmt analysis procedure, necessary to build a decode table - Create an invalid instruction to fallback on in case of unknown opcode - Utils to extract string representation of some objects' fields

--- cgen/ir-gen.scm
+++ cgen/ir-gen.scm
@@ -8,7 +8,8 @@
 (set! APPLICATION 'IR-GEN)
 
 ; Sanitize an element name to be a valid C++ variable name.
-; @param name must be a valid string or a symbol
+; NAME must be a valid string or a symbol
+
 (define (sanitize-elm-name name)
   (let ((str ""))
     (if (string? name)
@@ -25,7 +26,74 @@
     )
     (string->symbol str)
   )
-) 
+)
+
+; Returns the class name of INSN in the generated C++ translator.
+; E.g.: 
+; INSN is a "addi" -> AddiInstruction
+; INSN is a "x-invalid" -> EmptyInstruction
+
+(define (gen-insn-class-name insn)
+  (assert (insn? insn))
+
+  (if (eq? (obj:name insn) 'x-invalid)
+    "EmptyInstruction"
+    (string-append 
+      (string-capitalize (symbol->string (obj:name insn)))
+      "Instruction"
+    )
+  )
+)
+
+; Returns the class name of SFMT in the generated C++ translator.
+; E.g.: 
+; SFMT is a "sfmt-addi" -> AddiInstruction
+; SFMT is a "sfmt-empty" -> EmptyInstruction
+
+(define (gen-sfmt-class-name sfmt)
+  (assert (class-instance? <sformat> sfmt))
+
+  (let ((name (symbol->string (obj:name sfmt))))
+    (string-append
+      (string-capitalize (substring name (string-length "sfmt-")))
+      "Instruction"
+    )
+  )
+)
+
+; Returns the C++ var type representing the class of <mode> MODE
+
+(define (gen-mode-cpp-type mode)
+  (let* ((class (mode:class mode))
+    (ptr-to (elm-xget mode 'ptr-to)))
+      (if ptr-to
+        (string-append (gen-mode-cpp-type ptr-to) " *")
+        (cond 
+          ((eq? class 'INT) "int")
+          ((eq? class 'UINT) "unsigned int")
+          ((eq? class 'FLOAT) "float")
+          (else "int")
+        )
+      )
+  )
+)
+
+; Returns the C++ var type to contain <ifield> ifld
+
+(define (gen-ifld-cpp-type ifld)
+  (assert (ifield? ifld))
+
+  (gen-mode-cpp-type (ifld-decode-mode ifld))
+)
+
+; Scan insns, analyzing semantics and computing instruction formats.
+; 'twould be nice to do this in sim-analyze! but it doesn't know whether this
+; needs to be done or not (which is determined by what files are being
+; generated).  Since this is an expensive operation, we defer doing this
+; to the files that need it.
+
+; Only run sim-analyze-insns! once.
+(define -ir-gen-insns-analyzed? #f)
 
 ; Requested to be provided by "set-cgen-options!"
 (define (option-init!)
@@ -43,11 +111,48 @@
 )
 
 (define (ir-gen-finish!)
-	; Placeholder
+  ; Define invalid handler if not provided. For the moment, 'x-invalid
+  ; does nothing, the only purpose to have it, until semantic generation
+  ; is provided, is for decoding, where we should have a handler for possible
+  ; invalid instructions
+
+  (let ((all (stringize (current-arch-isa-name-list) ",")))
+    (logit 3 "Defining 'x-invalid insn\n")
+    (define-full-insn 'x-invalid "invalid insn handler"
+        `(VIRTUAL (ISA ,all))
+        "--invalid--" '() '() (list 'c-code 'VOID (string-append "\
+    { }"
+    )) nil)
+    )
+
 	*UNSPECIFIED*
 )
 
 (define (ir-gen-analyze!)
 	; Placeholder
 	*UNSPECIFIED*
+)
+
+(define (ir-gen-analyze-insns!)
+  ; This can only be done if one isa and one cpu family is being kept.
+  (assert-keep-one)
+
+  (if (not -ir-gen-insns-analyzed?)
+
+      (begin
+  (arch-analyze-insns! CURRENT-ARCH
+           #f ; don't include aliases
+           #t) ; do analyze the semantics
+
+  ; Compute the set of sformat argument buffers.
+  ; --- Removed for now. One thing at the time
+  ;(set! -ir-gen-sformat-abuf-list (compute-sformat-argbufs! (current-sfmt-list)))
+
+  (set! -ir-gen-insns-analyzed? #t)))
+
+  ; Do our own error checking.
+  ; --- Perform more research when implementing sem generation
+  ;(assert (current-insn-lookup 'x-invalid))
+
+  *UNSPECIFIED*
 )
